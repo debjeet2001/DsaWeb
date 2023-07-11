ac81ba19c0838a39650cbf553a691e4f9a76ed14
DEBJEET CHAKRABORTY <debjeet.chakraborty.2001@gmail.com>
2023-07-11 17:51:56 +0530

    TypeCasting.java modified

--- TypeCasting.java
+++ TypeCasting.java
@@ -4,7 +4,7 @@ public class TypeCasting{
 
     public static void main(String[] args){
         char ch = 'H';
-        short i =100;
+        short i =100; //compiler error, char doesnot hold negative values as short does. type casting error.
         ch = i;
         System.out.println(ch);
     }
