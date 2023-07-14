38acd6f74065e0c801bd47c06ea56ab81a04940a
DEBJEET CHAKRABORTY <debjeet.chakraborty.2001@gmail.com>
2023-07-14 21:31:58 +0530

    MirrorImagePattern.java added

--- MirrorImagePattern.java
+++ MirrorImagePattern.java
@@ -6,7 +6,17 @@ public class MirrorImagePattern {
         n = s.nextInt();
         //printing the pattern.
         for(int i=1;i<=n;i++){
-                        
+            int space=1;
+            while(space<= n-i ){
+                System.out.print(" ");
+                space++;
+            }
+            int star=1;
+            while(star<=i){
+                System.out.print("*");
+                star++;
+            }
+            System.out.println();            
         }
     }
 }
