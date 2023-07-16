27c5534b829dccf95487dd2567294e6cd526ed8d
DEBJEET CHAKRABORTY <debjeet.chakraborty.2001@gmail.com>
2023-07-16 21:07:34 +0530

    ParallelogramPattern.java added

--- ParallelogramPattern.java
+++ ParallelogramPattern.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class ParallelogramPattern {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        
+        int n;
+        n = s.nextInt();
+        //printing the Parallelogram pattern.
+        for(int i=1;i<=n;i++){
+            int space = i-1;
+            while(space>0){
+                System.out.print(" ");
+                space--;
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
