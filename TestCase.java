6904234ec691d4e998ff2755e6ea951e85acd156
DEBJEET CHAKRABORTY <debjeet.chakraborty.2001@gmail.com>
2023-08-05 00:40:44 +0530

    TestCase.java added

--- TestCase.java
+++ TestCase.java
@@ -7,17 +7,26 @@ public class TestCase {
         // }
         // System.out.print(i);
 
-        int[] arr = new int[5];
-        arr[0]=1;
-        arr[1]=2;
-        arr[2]=3;
-        arr[3]=4;
-        arr[4]=5;
-        System.out.print(arr[0]+ " " + arr[1] + " " + arr[2] + " " + arr[3] + " " +arr[4]);
-        arr = new int[6];
-        System.out.println();
-        System.out.print(arr[0]+ " " + arr[1] + " " + arr[2] + " " + arr[3] + " " +arr[4]);
+        // int[] arr = new int[5];
+        // arr[0]=1;
+        // arr[1]=2;
+        // arr[2]=3;
+        // arr[3]=4;
+        // arr[4]=5;
+        // System.out.print(arr[0]+ " " + arr[1] + " " + arr[2] + " " + arr[3] + " " +arr[4]);
+        // arr = new int[6];
+        // System.out.println();
+        // System.out.print(arr[0]+ " " + arr[1] + " " + arr[2] + " " + arr[3] + " " +arr[4]);
 
+        
+            System.out.println("Hello, World!");
+            for(int i=0;i<4;i++){
+                int count = i+1;
+                for(int j=0;j<4;j++){
+                    System.out.print(count++);
+                }
+                System.out.println();
+            }
         
 
     }
