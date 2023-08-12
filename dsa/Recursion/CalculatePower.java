39fd199e6bc5a1354dc3c7fb980befc1464198be
DEBJEET CHAKRABORTY <debjeet.chakraborty.2001@gmail.com>
2023-08-12 21:24:35 +0530

    dsa/Recursion/CalculatePower.java added

--- dsa/Recursion/CalculatePower.java
+++ dsa/Recursion/CalculatePower.java
@@ -1,5 +1,28 @@
 package dsa.Recursion;
 
+import java.util.Scanner;
+
 public class CalculatePower {
-    
+    public static int power(int x, int n) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+		 if(n==0)return 1;
+		 int smallOutput=power(x,n-1);
+		 
+		 int LargeOutput=x*smallOutput;
+
+		 return LargeOutput;
+		
+	}
+
+    public static void main(String[] args){
+        Scanner s  = new Scanner(System.in);
+        int x = s.nextInt();
+        int n = s. nextInt();
+        System.out.println(power(x,n));
+    }
 }
