d2f3f5ebdf9e222e848fda86129e35b8c2a4b96f
Debjeet Chakraborty <debjeet.chakraborty.2001@gmail.com>
2023-10-20 22:20:11 +0530

    modified UseGenerics.java

--- dsa/OOP/UseGenerics.java
+++ dsa/OOP/UseGenerics.java
@@ -1,14 +1,14 @@
 public class UseGenerics {
 
     public static void main(String[] args){
-        generics g = new generics(20,10);
+        generics g = new generics(20,10);//very dangerous avoid it.
         System.out.println(g.gettersfirst());
         System.out.println(g.getterssecond());
         g.setters(30, 20);
         System.out.println(g.gettersfirst());
         System.out.println(g.getterssecond());
 
-        generics h = new generics("debjeet","Chakraborty");
+        generics<String> h = new generics<String>("debjeet","Chakraborty");
         System.out.println(h.gettersfirst());
         System.out.println(h.getterssecond());
 
